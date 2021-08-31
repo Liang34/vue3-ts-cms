9d4110564482406a9eb9a4e8bcb087dd1a6316b8
Liang34 <51255027+Liang34@users.noreply.github.com>
2021-08-31 22:51:38 +0800

    '完成登录界面'

--- README.md
+++ README.md
@@ -1,24 +1,30 @@
 # vue3-ts-cms
 
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
+1、问题：normalize.css 与 element-plug 好像不兼容？我一起用会有问题，暂时用 reset.css 吧
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+// 重置样式
+import '@/assets/css/minireset.css'
+// element-plus样式
+import 'element-plus/dist/index.css'
+// 自定义样式
+import '@/assets/css/index.less'
+// 按需引入Element组件
+import { globalRegister } from '@/global'
 
-createApp(App).use(store).use(router).mount("#app");
+const app = createApp(App)
+app.use(store)
+app.use(router)
+app.use(globalRegister)
+app.mount('#app')
